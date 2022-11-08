310d22eb3043fe54448115d6db0397e15f420b84
Tushar98644 <tusharbanik@Tushars-Air.mshome.net>
2022-11-08 23:28:31 +0530

    p

---
+++ api/index.js
@@ -0,0 +1,41 @@
+const express =require('express');
+const app = express();
+
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGO_URL,{
+    useUnifiedTopology:true,
+    useNewUrlParser:true,
+    // useCreateIndex:true,
+
+})
+.then(()=>
+    console.log("success")
+)
+.catch((err)=>
+
+    console.log(err)
+)
+
+app.use(express.json());
+
+
+
+app.listen (8800,()=>
+{
+    console.log("success");
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
+
